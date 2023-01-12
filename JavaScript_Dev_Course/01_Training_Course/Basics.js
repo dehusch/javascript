2de782fdb038091726b4025cae5ea661f1e3a754
dehusch <denis.husch@gmail.com>
2023-01-12 20:34:13 -0300

    JavaScript Basics

--- JavaScript_Dev_Course/01_Training_Course/Basics.js
+++
@@ -1,7 +0,0 @@
-//JS Started
-//JSLint, Prettier and Node.JS must be installed.
-//Inside WebBrowser console, I can set up status and change const and functions status.
-//It's possible to change indentation size in status bar and set it up as you wish.
-
-//It's a better practice to put <script></script> at end of file in a HMTL file.
-//The reason is because the HTML must be render first before any script.
