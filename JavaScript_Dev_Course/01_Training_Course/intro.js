a98271148ff66ac0cd9f012840ed2e10d89bb9b6
dehusch <eberhard.husch@outlook.com>
2024-07-21 22:00:42 +0200

    add JS intro

---
+++ JavaScript_Dev_Course/01_Training_Course/intro.js
@@ -0,0 +1,31 @@
+/*
+ * 1. Introduction to JavaScript
+ * 2. JavaScript is a high-level, interpreted programming language
+ * 3. JavaScript is a multi-paradigm language
+ * 4. JavaScript is a dynamic language
+ * 5. JavaScript is a prototype-based language
+ * 6. JavaScript is a weakly typed language
+ * 7. JavaScript is a case-sensitive language
+ * 8. JavaScript is a client-side language
+ * 9. JavaScript is an object-oriented language
+ * 
+ * 
+*/
+
+//The console.log() method is used to log or print messages to the console. It can also be used to print objects and other info.
+
+console.log("Hello World");
+console.log(123);
+console.log(true);
+var greeting = "Hello";
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+console.error("This is an error message");
+//console.clear();
+console.warn("This is a warning message");
+console.time("Hello");
+console.log("Hello World");
+
+
