266cb0608d7eb06aa2242c14bb9358ad39ed2430
dehusch <eberhard.husch@outlook.com>
2024-07-23 23:27:28 +0200

    update

--- JavaScript_Dev_Course/01_Training_Course/intro.js
+++ JavaScript_Dev_Course/01_Training_Course/intro.js
@@ -35,3 +35,39 @@ console.log(0.2708 * 100);
 
 console.log('Hello' + 'World');
 console.log('Hello' + ' ' + 'World');
+console.log('Teaching the world how to code'.length);
+console.log('Codecademy'.toUpperCase());
+console.log('    Remove whitespace   '.trim());
+
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
+
+/*Let’s take one more glance at the concepts we just learned:
+
+    Data is printed, or logged, to the console, a panel that displays messages, 
+    with console.log().
+    
+    We can write single-line comments with // .
+    
+    There are 8 fundamental data types in JavaScript: numbers, BigInts, 
+    strings, booleans, null, undefined, symbol, and object.
+    
+    Numbers are any number without quotes: 23.8879
+    
+    Strings are characters wrapped in single or double quotes: 'Sample String'
+    
+    The built-in arithmetic operators include +, -, *, /, and %.
+    Objects, including instances of data types, can have properties, 
+    stored information. The properties are denoted with a . after the 
+    name of the object, for example: 'Hello'.length.
+    
+    Objects, including instances of data types, can have methods which 
+    perform actions. Methods are called by appending the object or instance 
+    with a period, the method name, and parentheses. For example: 'hello'.toUpperCase().
+    
+    We can access properties and methods by using the ., dot operator.
+    
+    Built-in objects, including Math, are collections of methods and 
+    properties that JavaScript provides.
+*/
